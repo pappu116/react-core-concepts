0743c41fb124b554a156a6a9ed3fa1b211455a06
pappu116 <52205941+pappu116@users.noreply.github.com>
2020-08-21 00:22:45 +0600

    similer-look

--- src/App.js
+++ src/App.js
@@ -23,12 +23,30 @@ var style={
         <p>
           Edit done mama <code>src/App.js</code> and save to reload.
         </p>
-  <h1 className="" style={style}>My Heading:{persone.name + " " + persone.job}</h1>
-  <h2 style={{backgroundColor:'skyblue', color:'#333'}}>singer: {persone2.name + " " + persone2.job}</h2>
-        <p>my frist react paragrah</p>
+         <h1 className="" style={style}>My Heading:{persone.name + " " + persone.job}</h1>
+         <h2 style={{backgroundColor:'skyblue', color:'#333'}}>singer: {persone2.name + " " + persone2.job}</h2>
+        <p>similler Look Style</p>
+        {/* similear look style */}
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person() {
+  var style={
+    border:'2px solid yellow',
+    margin:'10px'
+  }
+  return(
+    <div style={style}>
+       <h1>Name:Pappu Hasan</h1>
+       <h2>status: Learn React</h2>
+    </div>
+  )
 }
 
 export default App;
