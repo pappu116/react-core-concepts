d49f20275e6fffd3d244d6383005cb91dab90171
pappu116 <52205941+pappu116@users.noreply.github.com>
2020-08-20 23:44:06 +0600

    some-example

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  var persone = {
+      name:"Dr. MAhfuz",
+      job: "singer"
+  }
+  var persone2 = {
+    name:"eva rahman",
+    job: "kokil konthi"
+}
+var style={
+  color:'red',
+  backgroundColor:'yellow'
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +23,9 @@ function App() {
         <p>
           Edit done mama <code>src/App.js</code> and save to reload.
         </p>
-        
+  <h1 className="" style={style}>My Heading:{persone.name + " " + persone.job}</h1>
+  <h2 style={{backgroundColor:'skyblue', color:'#333'}}>singer: {persone2.name + " " + persone2.job}</h2>
+        <p>my frist react paragrah</p>
       </header>
     </div>
   );
